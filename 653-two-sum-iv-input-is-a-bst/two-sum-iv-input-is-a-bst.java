89753509f383eb90b7e01e050f59462ce3038ddb
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2023-12-20 12:39:27 +0530

    Time: 2 ms (95.56%) | Memory: 44.4 MB (17.89%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,34 +14,19 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root, List<Integer> ans) {
-        if(root == null) return;
-
-        inorder(root.left, ans);
-        ans.add(root.val);
-        inorder(root.right, ans);
-    }
+    
+    HashSet<Integer> set = new HashSet<>();
 
     public boolean findTarget(TreeNode root, int k) {
         if(root == null) return false;
-        List<Integer> list = new ArrayList<>();
-        inorder(root, list);
 
-        // Now the list contains inorder traversal values 
-        int start = 0;
-        int end = list.size()-1;
+        // check if the set contains "k - root.val" => if YES - true if NO - False
+        if(set.contains(k - root.val)) return true;
+        
+        // else it add the value to the set
+        set.add(root.val);
 
-        while(start < end) {
-            int sum = list.get(start) +  list.get(end);
-
-            if(sum < k) {
-                start++;
-            } else if(sum > k) {
-                end--;
-            } else {  // sum == k "Found the ans"
-                return true;
-            }
-        }
-        return false;  
+        // check for the left and right nodes and return ans
+        return findTarget(root.left, k) ||  findTarget(root.right,k);
     }
 }
