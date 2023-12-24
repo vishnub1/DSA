87f87a73d31db0aa4a2ed429cdaf6be05e046e64
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2023-12-24 13:04:47 +0530

    Time: 1 ms (99.99%) | Memory: 57.2 MB (90.99%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // edge case
-        int n = nums.length;
-        if(n == 1) return nums[0];
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
 
-        int maxSum = nums[0];  // ans
-        int train = nums[0];   // previous sum 
-
-        ///////// kadane's algorithm//////////
-        for(int i = 1; i < nums.length; i++) {
-            train = Math.max(nums[i], train + nums[i]);   // curr element will always wants to amplify 
-            maxSum = Math.max(maxSum, train);
+        for(int i = 0; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);  // curr sum will choose to start a new train or sit with previous train and amplify its value 
+            maxSum = Math.max(maxSum, currSum);
         }
+
         return maxSum;
     }
 }
