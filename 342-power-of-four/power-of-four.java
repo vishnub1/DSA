2cba558d7f4be14494ca8860d06d78f54095b52a
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2023-12-27 15:52:41 +0530

    Time: 1 ms (74.69%) | Memory: 40.8 MB (6.04%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        while(n > 1) {
+            if(n % 4 != 0) {
+                return false;
+            }
+            n = n / 4;
+        }
+        if(n == 1) return true;
+        return false;
+    }
+}
