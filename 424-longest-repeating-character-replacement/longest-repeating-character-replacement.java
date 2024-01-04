8d4045cf2d6fafc48998431a0b621170931c3c9b
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-01-04 15:48:08 +0530

    Time: 9 ms (71.89%) | Memory: 42.3 MB (50.90%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,34 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // Make an array of size 26
-        int arr[] = new int[26];
+        int[] charCount = new int[26]; // to store the count of characters in the current window
+        int maxCount = 0; // to keep track of the maximum repeating character count in the current window
+        int maxLength = 0; // to store the length of the longest substring containing the same letter
 
-        // intitialize largestCount, maxlen, & beg pointer
-        int largestCount = 0;
-        int start = 0;
-        int maxLen = 0;
+        int start = 0; // start of the window
 
-        // Traverse all character through the loop
-        for(int end = 0; end < s.length(); end++) {
-            arr[s.charAt(end) - 'A']++;  // freq array
+        for (int end = 0; end < s.length(); end++) {
+            char currentChar = s.charAt(end);
+            charCount[currentChar - 'A']++; // increment count of the current character in the window
+            maxCount = Math.max(maxCount, charCount[currentChar - 'A']); // update maxCount
 
-            // Get the largest count of a single, unique character int the current window
-            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
-
-            // we are allowed to have at most k replacements in the window 
-            // so, if max character frequency + distance between start and end is greater than k
-            // this means we have considered changing more than k characters, so time to shrink the window
-            // Then there are more characters in the window than we can relpace and we need to shrinkthe window
-            // ( window length - largestCount  > k )
-            if(end-start+1 - largestCount > k) {
-                arr[s.charAt(start) - 'A']--;
-                start++;
+            // If the size of the window (end - start + 1) minus maxCount is greater than k,
+            // shrink the window from the left side
+            if (end - start + 1 - maxCount > k) {
+                charCount[s.charAt(start) - 'A']--; // decrement count of the character going out of the window
+                start++; // move the window to the right
             }
 
-            // get the maximum length of repeating character 
-            maxLen = Math.max(maxLen, end-start+1);
+            // Update maxLength with the current window size
+            maxLength = Math.max(maxLength, end - start + 1);
         }
 
-        return maxLen;
+        return maxLength;
     }
 }
