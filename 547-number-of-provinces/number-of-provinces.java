c27b94fd35e3b8bec278063fe7aac51705ab5788
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-01-11 00:01:48 +0530

    Time: 1 ms (95.21%) | Memory: 47.9 MB (16.08%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,22 +1,27 @@
-public class Solution {
-    public int findCircleNum(int[][] M) {
-        boolean[] visited = new boolean[M.length]; //visited[i] means if ith person is visited in this algorithm
-        int count = 0;
+class Solution {
+
+    public static void dfs(int[][] M, boolean visited[], int node) {
+        visited[node] = true;
+
         for(int i = 0; i < M.length; i++) {
-            if(!visited[i]) {
-                dfs(M, visited, i);
-                count++;
+            if(M[node][i] == 1 && !visited[i]) {
+                dfs(M, visited, i); // traverse for nbrs
             }
         }
-        return count;
     }
-    private void dfs(int[][] M, boolean[] visited, int person) {
-        for(int other = 0; other < M.length; other++) {
-            if(M[person][other] == 1 && !visited[other]) {
-                //We found an unvisited person in the current friend cycle 
-                visited[other] = true;
-                dfs(M, visited, other); //Start DFS on this new found person
+
+    public int findCircleNum(int[][] M) {
+        int n = M.length;
+        int count = 0;
+        boolean visited[] = new boolean[n];
+
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(M, visited, i);
             }
         }
+
+        return count;
     }
 }
