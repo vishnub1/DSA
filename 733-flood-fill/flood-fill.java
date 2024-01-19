e3425af415c23735ca44b05fbcf21fc3a7c11842
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-01-19 16:20:11 +0530

    Time: 1 ms (49.26%) | Memory: 45.2 MB (7.53%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,28 +1,46 @@
 class Solution {
-    public void dfs(int[][] image, int r, int c, int color, int newColor) {
-        if(image[r][c] == color){  // color is the first color at {sr,sc,} before chinging
-            image[r][c] = newColor;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int originalColor = image[sr][sc];
 
-            if(r >= 1) {
-                dfs(image, r - 1, c, color, newColor); // left
-            }
-            if(c >= 1) {
-                dfs(image, r, c - 1, color, newColor); // up 
-            }
-            if(r+1 < image.length) {
-                dfs(image, r+1, c, color, newColor);
-            }
-            if(c+1 < image[0].length) {
-                dfs(image, r, c + 1, color, newColor);
-            }
+        // Check if the starting pixel color is different from the new color
+        if (originalColor != color) {
+            bfs(image, sr, sc, originalColor, color);
         }
-    }
-    
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int color = image[sr][sc];
-        if(color != newColor) {
-            dfs(image, sr, sc, color, newColor);
-        }
+
         return image;
+    }
+
+    private void bfs(int[][] image, int sr, int sc, int originalColor, int newColor) {
+        int rows = image.length;
+        int cols = image[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // 4-directions: right, left, down, up
+
+        while (!queue.isEmpty()) {
+            int[] currentPixel = queue.poll();
+            int row = currentPixel[0];
+            int col = currentPixel[1];
+
+            // Check if the current pixel has the same color as the starting pixel
+            if (image[row][col] == originalColor) {
+                // Update the color of the current pixel
+                image[row][col] = newColor;
+
+                // Explore the 4-directionally connected pixels
+                for (int[] direction : directions) {
+                    int newRow = row + direction[0];
+                    int newCol = col + direction[1];
+
+                    // Check if the new position is within the image boundaries
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                        // Add the 4-directionally connected pixels to the queue
+                        queue.offer(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
     }
 }
