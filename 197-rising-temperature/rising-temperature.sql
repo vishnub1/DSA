65c4b31a943e4c2468fe934850ca159b50db6f29
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-01-22 10:45:07 +0530

    Time: 1289 ms (13.53%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -3,5 +3,5 @@
 -- self join
 SELECT w1.id
 FROM Weather w1
-JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+JOIN Weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
 WHERE w1.temperature > w2.temperature;
