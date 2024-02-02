0052bbd0f593edb01a32c2257701cb6a26000699
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-02-02 22:01:20 +0530

    Time: 38 ms (56.47%) | Memory: 59.9 MB (5.24%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,3 +1,78 @@
+class Node {
+    Node links[] = new Node[26];
+    boolean flag = false;
+
+    public Node() {
+    
+    }
+
+    boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    Node get(char ch) {
+        return links[ch - 'a']; 
+    }
+
+    void put(char ch, Node node) {
+        links[ch - 'a'] = node;
+    }
+    
+    void setEnd() {
+        flag = true;
+    }
+
+    boolean isEnd(){
+        return flag;
+    }
+
+}
+
+class Trie {
+    private static Node root;
+    public Trie() {
+        root = new Node();
+    }
+    
+    // Insert
+    public void insert(String word) {
+        Node node = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(!node.containsKey(word.charAt(i))) {
+                node.put(word.charAt(i), new Node());
+            }
+            node = node.get(word.charAt(i));
+        }
+        node.setEnd();
+    }
+    
+    // Search
+    public boolean search(String word) {
+        Node node = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(!node.containsKey(word.charAt(i))) {
+                return false;
+            }
+            node = node.get(word.charAt(i));
+        }
+        if(node.isEnd()) {
+            return true; // word found
+        }
+        return false; // Not found
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node node = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            if(!node.containsKey(prefix.charAt(i))) {
+                return false;
+            }
+            node = node.get(prefix.charAt(i));
+        }
+        return true;
+    }
+}
+
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie obj = new Trie();
@@ -5,79 +80,3 @@
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
-class Node {
-    Node links[] = new Node[26];
-    boolean flag = false;
-
-    public Node() {
-
-    }
-
-    boolean containsKey(char ch) {
-        return (links[ch - 'a'] != null);
-    }
-
-    Node get(char ch) {
-        return links[ch - 'a'];
-    }
-
-    void put(char ch, Node node) {
-        links[ch - 'a'] = node;
-    }
-
-    void setEnd() {
-        flag = true;
-    }
-
-    boolean isEnd() {
-        return flag;  // falg returning 
-    }
-}
-
-class Trie {
-    private static Node root;
-
-    // initialize your data structure here
-    Trie() {
-        root = new Node();
-    }
-
-    // Insert a word into the trie
-    public void insert(String word) {
-        Node node = root;
-        for (int i = 0; i < word.length(); i++) {
-            if (!node.containsKey(word.charAt(i))) { // create and put
-                node.put(word.charAt(i), new Node());
-            }
-            node = node.get(word.charAt(i));
-        }
-        node.setEnd(); // falg true
-    }
-
-    // Returns if the word is in the trie "Complete word as it is"
-    public boolean search(String word) {
-        Node node = root;
-        for (int i = 0; i < word.length(); i++) {
-            if (!node.containsKey(word.charAt(i))) {
-                return false;
-            }
-            node = node.get(word.charAt(i)); // when this stop at last and flag is true the ans is true
-        }
-        if (node.isEnd()) {
-            return true; // word is presnet
-        }
-        return false;
-    }
-
-    // returns if there is any word in the trie that starts with the given prefix
-    public boolean startsWith(String prefix) {
-        Node node = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            if (!node.containsKey(prefix.charAt(i))) {
-                return false;
-            }
-            node = node.get(prefix.charAt(i));
-        }
-        return true; // yaha tak aaya to true hoga nahi to upper is false ho jata
-    }
-}
