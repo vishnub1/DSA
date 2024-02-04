5b14fb58c48625e8791a1548cc4b43abd2f0bf05
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-02-04 09:48:11 +0530

    Time: 1 ms (80.73%) | Memory: 42.2 MB (9.25%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,24 +1,85 @@
-import java.util.Arrays;
+public class TrieNode {
+    TrieNode[] links = new TrieNode[26];
+    boolean isEnd;
+    int size;
+
+    public TrieNode() { }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+        size++;
+    }
+
+    public int getLinks() {
+        return size;
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
 
 class Solution {
-    
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) return "";
-
-        Arrays.sort(strs);
-
-        String first = strs[0];
-        String last = strs[strs.length-1];
-
-        StringBuilder result = new StringBuilder();
-
-        for(int i = 0; i < first.length() && i < last.length(); i++) {
-            if(first.charAt(i) != last.charAt(i)) {
-                break;
-            }
-            result.append(first.charAt(i));
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
         }
 
-        return result.toString();
+        Trie trie = new Trie();
+        for (int i = 0; i < strs.length; i++) {
+            trie.insert(strs[i]);
+        }
+        return trie.searchLongestPrefix(strs[0]);
+    }
+
+    public class Trie {
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        // insert
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                if (!node.containsKey(ch)) {
+                    node.put(ch, new TrieNode());
+                }
+                node = node.get(ch);
+            }
+            node.setEnd();
+        }
+
+        // Search longestPrefix
+        public String searchLongestPrefix(String word) {
+            TrieNode node = root;
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if (node.getLinks() == 1 && !node.isEnd()) { // checking if one child is present
+                    ans.append(ch);
+                    node = node.get(ch);
+                } else {
+                    return ans.toString();
+                }
+            }
+            return ans.toString();
+        }
     }
 }
