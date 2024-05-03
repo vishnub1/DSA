5aecbaf2d42abfd406790cdb02e79f185d725f11
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-05-03 16:36:46 +0530

    Time: 29 ms (54.64%) | Memory: 87.5 MB (84.36%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,41 +1,41 @@
+
+
 class RandomizedSet {
     ArrayList<Integer> list;
     Map<Integer, Integer> map;
 
     public RandomizedSet() {
-        list = new ArrayList<>();    
-        map = new HashMap<>();    
+        list = new ArrayList<>();
+        map = new HashMap<>();
     }
-    
+
+    public boolean search(int val) {
+        return map.containsKey(val);
+    }
+
     public boolean insert(int val) {
-        if(map.containsKey(val)) return false; // already present
+        if (search(val)) return false;
+
         list.add(val);
-        map.put(val, list.size()-1);  // (val, index)
+        map.put(val, list.size() - 1);
         return true;
     }
-    
+
     public boolean remove(int val) {
-        if(map.containsKey(val) == false) return false; // not present
+        if (!search(val)) return false;
+
         int index = map.get(val);
-        Collections.swap(list, index, list.size()-1);
-        int swappedWith = list.get(index);
-        map.put(swappedWith, index);
-        list.remove(list.size()-1);
+        list.set(index, list.get(list.size() - 1));
+        map.put(list.get(index), index);
+        list.remove(list.size() - 1);
         map.remove(val);
+
         return true;
     }
-    
+
     public int getRandom() {
-        Random random = new Random();
-        int n = random.nextInt(list.size());
-        return list.get(n);
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
     }
 }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
