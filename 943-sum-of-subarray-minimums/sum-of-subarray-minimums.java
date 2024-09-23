e9e276ed64a7722f01b7eece4341d37be26b862e
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-09-23 09:52:38 +0530

    Time: 74 ms (25.73%) | Memory: 50.9 MB (61.38%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,51 +1,64 @@
 class Solution {
-    public int sumSubarrayMins(int[] a) {
-        int n = a.length;
-        Stack<Integer> st = new Stack<>();
-		
-        // nsl - left Boundary 
-        int nsl[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            while(!st.isEmpty() && a[i] < a[st.peek()]) {
-                st.pop();
-            }
-            if(st.isEmpty()) {
-                nsl[i] = -1;
-            } else {
-                nsl[i] = st.peek();
-            }
-            st.push(i);
+    public int sumSubarrayMins(int[] arr) {
+        int nextSm[] = nextSmaller(arr);
+        int prevSm[] = prevSmaller(arr);
+
+        long ans = 0;
+        int mod = 1000000007;
+
+        for (int i = 0; i < arr.length; i++) {
+            // Number of subarrays where arr[i] is the minimum
+            long first = nextSm[i] - i;
+            long second = i - prevSm[i];
+
+            // Calculate the contribution of arr[i] as the minimum in the subarrays
+            long product = (first * second) % mod;
+            product = (product * arr[i]) % mod;
+
+            // Add to the final answer
+            ans = (ans + product) % mod;
+
         }
 
-        st = new Stack<>();
-        // nsr - Right Boundary 
-        int nsr[] = new int[n];
-        for(int i = n-1; i >= 0; i--) {
-            while(!st.isEmpty() && a[i] <= a[st.peek()]) {
-                st.pop();
-            }
-            if(st.isEmpty()) {
-                nsr[i] = n;
-            } else {
-                nsr[i] = st.peek();
-            }
-            st.push(i);
-        }
-
-        long mod = 1000000007;
-        long totalSum = 0;
-        for(int i = 0; i < n; i++) {
-            long left = i - nsl[i];
-            long right = nsr[i] - i;
-
-            // (a*b)%m = (a%m * b%m)%m (Modulus Property)
-            long numberOfSubarray = (left % mod * right % mod) % mod;
-            long sum = (a[i] % mod * numberOfSubarray % mod) % mod;
-
-            // (a+b)%m = (a%m + b%m)%m (Modulus Property)
-            totalSum = (totalSum % mod + sum % mod) % mod;
-        }
-
-        return (int)totalSum;
+        return (int) ans;
     }
+
+    // Next Smaller
+    private int[] nextSmaller(int arr[]) {
+        Stack < Integer > st = new Stack < > ();
+        int nse[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                nse[i] = arr.length;
+            } else {
+                nse[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return nse;
+    }
+
+    // Previous Smaller
+    private int[] prevSmaller(int arr[]) {
+        Stack < Integer > st = new Stack < > ();
+        int pse[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                pse[i] = -1;
+            } else {
+                pse[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return pse;
+    }
+
 }
