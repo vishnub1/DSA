db53c6130e02870f9f4523356dd3aa573196fa37
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-10-01 10:49:24 +0530

    Time: 31 ms (45.66%) | Memory: 45 MB (68.86%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -5,17 +5,17 @@ class Solution {
         // Count occurrences of each character
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        // returing the index of first non repeating char
-        int idx = -1;
+         // returing the index of first non repeating char
         for(int i = 0; i < s.length(); i++) {
             if(map.get(s.charAt(i)) == 1) {
-                idx = i;
                 return i;
             }
         }
-        return idx;
+
+        return -1;
     }
 }
