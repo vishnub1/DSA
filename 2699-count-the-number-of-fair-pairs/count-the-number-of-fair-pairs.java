3f13b4c4af48c7051e4309bbb4eb459c2120056c
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2024-11-13 22:54:05 +0530

    Time: 24 ms (81.88%) | Memory: 57.2 MB (66.19%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,43 +1,27 @@
 class Solution {
 
-    long lower_bound(int[] nums, int low, int high, int element) {
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (nums[mid] >= element) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
     }
 
-    long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);
-        long ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            // Assume we have picked nums[i] as the first pair element.
-
-            // `low` indicates the number of possible pairs with sum < lower.
-            long low = lower_bound(
-                nums,
-                i + 1,
-                nums.length - 1,
-                lower - nums[i]
-            );
-
-            // `high` indicates the number of possible pairs with sum <= upper.
-            long high = lower_bound(
-                nums,
-                i + 1,
-                nums.length - 1,
-                upper - nums[i] + 1
-            );
-
-            // Their difference gives the number of elements with sum in the
-            // given range.
-            ans += 1 * (high - low);
+    // Calculate the number of pairs with sum less than `value`.
+    private long lower_bound(int[] nums, int value) {
+        int left = 0, right = nums.length - 1;
+        long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            // If sum is less than value, add the size of window to result and move to the
+            // next index.
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                // Otherwise, shift the right pointer backwards, until we get a valid window.
+                right--;
+            }
         }
-        return ans;
+
+        return result;
     }
 }
